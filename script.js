aad9ab594866d50aeeaf7f8e8471a8fcc5a5b271
Shola Ayeni <ayenisholah@yahoo.com>
2019-07-22 14:00:31 +0100

    set up

---
+++ script.js
@@ -0,0 +1,8 @@
+let card1 = 'Aces of Spades',
+    card2 = 'Ten of Hearts';
+
+console.log('Welcome to BlackJack!');
+
+console.log("You're dealt: ");
+console.log(" " + card1);
+console.log(" " +card2);
